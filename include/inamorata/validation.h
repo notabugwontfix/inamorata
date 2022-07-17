ebd75d84bcc0f1d90962cc5a581f0fb1b82727da
Not-a-Bug Won't Fix <notabugwontfix@proton.me>
2022-07-17 03:51:37 +0300

    Add source file stubs

---
+++ include/inamorata/validation.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <istream>
+#include <vector>
+
+namespace inamorata {
+
+void validate(const std::istream &source, const std::istream &schema);
+
+} // namespace inamorata
--- src/validation.cpp
+++ src/validation.cpp
@@ -1,3 +1,4 @@
 #include <doctest/doctest.h>
+#include <yaml-cpp/yaml.h>
 
 TEST_CASE("test") { CHECK(1 == 1); }
