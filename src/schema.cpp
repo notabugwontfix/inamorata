4cc6452a90c1e6d2f57e7faf6ffde60758030c81
Not-a-Bug Won't Fix <notabugwontfix@proton.me>
2022-07-19 05:48:34 +0300

    Schema implementation

--- src/schema.cpp
+++ src/schema.cpp
@@ -2,4 +2,23 @@
 
 #include <doctest/doctest.h>
 
-namespace inamorata {}
+namespace inamorata {
+
+Schema::Schema(std::istream &schema) {
+  const auto node = YAML::Load(schema);
+
+  m_root = node["root"];
+  m_types = node["types"].as<decltype(m_types)>();
+}
+
+auto Schema::type(const std::string &type) -> const YAML::Node * {
+  const auto node = m_types.find(type);
+
+  if (node != m_types.end()) {
+    return &node->second;
+  }
+
+  return nullptr;
+}
+
+} // namespace inamorata
--- src/schema.h
+++ src/schema.h
@@ -2,4 +2,22 @@
 
 #include <yaml-cpp/yaml.h>
 
-namespace inamorata {}
+#include <istream>
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+namespace inamorata {
+
+class Schema {
+public:
+  explicit Schema(std::istream &schema);
+
+  auto type(const std::string &type) -> const YAML::Node *;
+
+private:
+  YAML::Node m_root;
+  std::unordered_map<std::string, YAML::Node> m_types;
+};
+
+} // namespace inamorata
